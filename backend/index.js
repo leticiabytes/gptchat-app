91b53db2bd71c640f76bb1b4fdec0b0c18bc1c80
Letícia Vieira <lvieira268@gmail.com>
2023-01-10 20:06:31 -0300

    feat: add dotenv config
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
 
 const PORT = 3000;
 const app = express();
@@ -9,3 +10,10 @@ app.post('/gpt-request', async (resquest, response, next) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+dotenv.config({path: '../.env'});
+
+const OPENAI_AUTHORIZATION_KEY = process.env.OPENAI_AUTHORIZATION_KEY;
+const OPENAI_COOKIE = process.env.OPENAI_COOKIE;
+const STREAM_API_KEY = process.env.STREAM_API_KEY;
+const STREAM_API_SECRET = process.env.STREAM_API_SECRET;
