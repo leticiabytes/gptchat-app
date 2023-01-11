f20a0dc5914e577b415806a7d7b8ee0c5868c3bb
Letícia Vieira <lvieira268@gmail.com>
2023-01-11 20:05:30 -0300

    feat: add configureStream function
    Signed-off-by: Letícia Vieira <lvieira268@gmail.com>

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,22 @@
+
 import express from 'express';
+import * as dotenv from 'dotenv';
 
-const PORT = 3000;
+import { StreamChat } from 'stream-chat';
+import { configureStream } from './stream_setup.js';
+
+dotenv.config({path: '../.env'});
+
+const OPENAI_AUTHORIZATION_KEY = process.env.OPENAI_AUTHORIZATION_KEY;
+const OPENAI_COOKIE = process.env.OPENAI_COOKIE;
+const STREAM_API_KEY = process.env.STREAM_API_KEY;
+const STREAM_API_SECRET = process.env.STREAM_API_SECRET;
+
+const serverClient = StreamChat.getInstance(STREAM_API_KEY, STREAM_API_SECRET);
 const app = express();
+
+
+configureStream(serverClient).then(_ => console.log('Stream configured!'));
+
 app.use(express.json());
-
-app.post('/gpt-request', async (resquest, response, next) => {
-	response.status(200);
-});
-
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(3000, () => console.log('Server listening on port 3000!'));
